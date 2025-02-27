4166a05b04609eb33856e27016f77888b8f60e35
Charlie Jackson <cgj@cgjmedia.uk>
2025-02-27 13:35:49 +0000

    Add login.js file

---
+++ client/loginPage/login.js
@@ -0,0 +1,27 @@
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.target);
+
+    const options = {
+        method: "POST",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: form.get("email"),
+            password: form.get("password")
+        })
+    }
+
+    const response = await fetch("", options);
+    const data = await response.json();
+
+    if (response.status == 200) {
+        localStorage.setItem("token", data.token)
+        window.location.assign("./shopPage/shop.html")
+    } else {
+        alert(data.error);
+    }
+})
