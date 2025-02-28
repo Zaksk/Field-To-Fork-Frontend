8282a3e94e356a0a33164c5b54a37c683745b99e
Zaksk <zakariamar.99@gmail.com>
2025-02-28 14:14:50 +0000

    Connect login to backend

--- client/loginPage/login.js
+++ client/loginPage/login.js
@@ -11,17 +11,18 @@ document.getElementById("loginForm").addEventListener("submit", async (e) => {
         },
         body: JSON.stringify({
             email: form.get("email"),
-            password: form.get("password")
+            password_hash: form.get("password")
         })
-    }
+    };
 
-    const response = await fetch("", options);
+    const response = await fetch("https://field-to-fork-backend.onrender.com/users/login", options);
     const data = await response.json();
+    console.log(data)
 
-    if (response.status == 200) {
-        localStorage.setItem("token", data.token)
-        window.location.assign("./shopPage/shop.html")
+    if (response.ok) {
+        localStorage.setItem("token", data.token);
+        window.location.assign("../shopPage/shop.html");
     } else {
         alert(data.error);
     }
-})
+});
